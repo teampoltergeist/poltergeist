892d48b6f9619c2838a50d3e3c55c78d274d6756
Jon Leighton <j@jonathanleighton.com>
2011-10-28 18:52:06 +0100

    Change this test yet again to not rely on ps, and make it allow up to a second for the executable to be called.

--- spec/integration/driver_spec.rb
+++ spec/integration/driver_spec.rb
@@ -12,11 +12,23 @@ describe Capybara::Poltergeist::Driver do
   it_should_behave_like "driver with cookies support"
 
   it 'should support a custom phantomjs path' do
-    path = File.expand_path('../../support/custom_phantomjs', __FILE__)
+    file = File.expand_path('../../support/custom_phantomjs_called', __FILE__)
+    path = File.expand_path('../../support/custom_phantomjs',        __FILE__)
+
+    FileUtils.rm_f file
 
     driver  = Capybara::Poltergeist::Driver.new(nil, :phantomjs => path)
     driver.browser
 
-    `ps -o command=`.should include(path)
+    # If the correct custom path is called, it will touch the file. We allow at
+    # least 1 sec for this to happen before failing.
+
+    tries = 0
+    until File.exist?(file) || tries == 10
+      sleep 0.1
+      tries += 1
+    end
+
+    File.exist?(file).should == true
   end
 end
