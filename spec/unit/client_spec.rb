3645c3e295268817fe58d311519278b02dd2fff0
Jon Leighton <j@jonathanleighton.com>
2012-02-29 11:25:43 +0000

    Add test for #22. Closes #22.

--- spec/unit/client_spec.rb
+++ spec/unit/client_spec.rb
@@ -8,5 +8,14 @@ module Capybara::Poltergeist
       subject.stub(:`).with('phantomjs --version').and_return("1.3.0\n")
       expect { subject.start }.to raise_error(PhantomJSTooOld)
     end
+
+    it 'shows the detected version in the version error message' do
+      subject.stub(:`).with('phantomjs --version').and_return("1.3.0\n")
+      begin
+        subject.start
+      rescue PhantomJSTooOld => e
+        e.message.should include('1.3.0')
+      end
+    end
   end
 end
