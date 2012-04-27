7cb46fbc3c002c062bb11953ffa46f6ab2c7bca6
Dmitriy Nesteryuk <nesterukd@gmail.com>
2012-04-28 00:54:47 +0300

    added javascript code to fire keyup/keydown/keypress/focus/blur events

--- spec/support/public/test.js
+++ spec/support/public/test.js
@@ -3,7 +3,27 @@ $(function() {
     $('#remove_me').remove()
   })
 
-  $('#change_me').change(function(event) {
-    $('#changes').text($('#change_me').val())
-  })
+  var increment = function(index, oldText) {
+    return parseInt(oldText || 0) + 1;
+  }
+
+  $('#change_me')
+    .change(function(event) {
+      $('#changes').text($(this).val())
+    })
+    .keydown(function(event) {
+      $('#changes_on_keydown').text(increment)
+    })
+    .keyup(function(event) {
+      $('#changes_on_keyup').text(increment)
+    })
+    .keypress(function() {
+      $('#changes_on_keypress').text(increment)
+    })    
+    .focus(function(event) {
+      $('#changes_on_focus').text('Focus')
+    })
+    .blur(function() {
+      $('#changes_on_blur').text('Blur')
+    })
 })
