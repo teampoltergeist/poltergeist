d303963170b6f5d248b4ddfe154804306f3723bf
Jon Leighton <j@jonathanleighton.com>
2013-01-26 22:20:02 +0000

    Tweak the error

--- lib/capybara/poltergeist/errors.rb
+++ lib/capybara/poltergeist/errors.rb
@@ -60,8 +60,9 @@ module Capybara
 
     class ObsoleteNode < NodeError
       def message
-        "The element you are trying to interact with is not part of the DOM. " \
-          "It's possible to element has been replaced by another element and you meant to interact with " \
+        "The element you are trying to interact with is either not part of the DOM, or is " \
+          "not currently visible on the page (perhaps display: none is set). " \
+          "It's possible the element has been replaced by another element and you meant to interact with " \
           "the new element. If so you need to do a new 'find' in order to get a reference to the " \
           "new element."
       end
